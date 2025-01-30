1a9e6280147ae7036dad7a9eb6e033ce2b9cfb1f
Lucas Venancio <94983682+WebLucasDev@users.noreply.github.com>
2025-01-30 18:07:48 -0300

    commit 30-01-2025 -2
    - Continuidade no componente de opções

--- tcc-app/app/(stack)/(tabs)/index.tsx
+++ tcc-app/app/(stack)/(tabs)/index.tsx
@@ -142,6 +142,6 @@ const styles = StyleSheet.create({
   botaoAjustePontoTexto:{
     textAlign:'center',
     fontSize:12
-  }
+  },
 
 })
--- tcc-app/components/UsuarioOpecoes.tsx
+++ tcc-app/components/UsuarioOpecoes.tsx
@@ -1,34 +1,53 @@
-import { Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Modal, StyleSheet, Text, TouchableOpacity, View, Animated, Easing } from 'react-native'
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Constants from '@/constants/Constants';
 
 const UsuarioOpecoes = () => {
 
     const [visivel, setVisivel] = useState(false)
+    const escala = useRef(new Animated.Value(0)).current;
     const opcoes = [
         {
             titulo: 'Sair',
-            acao: () => alert('calma calabreso')
+            acao: () => console.log('calma calabreso')
         }
     ]
+    const teste =  console.log('calma calabreso');
+
+    function redefinirEscala(para:number){
+        if (para === 1){
+            setVisivel(true)
+        }
+
+        Animated.timing(escala,{
+            toValue: para,
+            useNativeDriver: true,
+            duration: 200,
+            easing: Easing.linear,
+        }).start(() => {
+            if (para === 0){
+                setVisivel(false)
+            }
+        })
+    }
 
     return (
-    <TouchableOpacity onPress={() => setVisivel(true)}>
+    <TouchableOpacity onPress={() => redefinirEscala(1)}>
         <FontAwesome name="user" size={24} color={Constants.LARANJA} />
         <Modal transparent visible={visivel}>
 
             <SafeAreaView style={styles.safeArea} onTouchStart={() => setVisivel(false)}>
 
-                <View style={styles.containerOpcoes}>
+                <Animated.View style={styles.containerOpcoes}>
                     {opcoes.map ((op, i) =>(
-                        <TouchableOpacity style={styles.botaoOpcoes} key={i} onPress={() => op.acao}>
+                        <TouchableOpacity style={styles.botaoOpcoes} key={i} onPress={() => teste}>
                             <Text>{op.titulo}</Text>
                             <FontAwesome name="sign-out" size={24} color={Constants.LARANJA} />
                         </TouchableOpacity>
                     ))}
-                </View>
+                </Animated.View>
 
             </SafeAreaView>
 
