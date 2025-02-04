9ddfc4efb12cd7a9816b073d76f2a1e33659e2ee
Lucas Venancio <weblucasdev@gmail.com>
2025-02-03 23:22:53 -0300

    commit 03-02-2025 -2
    - Finalização da tela de alteração de cadastros

--- tcc-app/app/(stack)/(tabs)/informacoesCadastrais.tsx
+++ tcc-app/app/(stack)/(tabs)/informacoesCadastrais.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import Constants from '@/constants/Constants'
@@ -29,11 +29,18 @@ export default function informacoesCadastrais() {
             <TextInput style={styles.dadosCadastraisInput} placeholder='Cargo:'/>
           </View>
 
-          {/* <View style={styles.containerDadosCadastraisInput}>
+          <View style={styles.containerDadosCadastraisInput}>
             <Text style={styles.dadosCadastraisLabel}>Horários de Trabalho:</Text>
-            <TextInput placeholder='08:00 - 11:00'/>
-            <TextInput placeholder='13:00 - 18:00'/>
-          </View> */}
+
+            <View style={styles.containerDadosCadastraisHorarios}>
+              <TextInput style={styles.dadosCadastraisHorariosInput} placeholder='08:00 - 11:00'/>
+              <TextInput style={styles.dadosCadastraisHorariosInput} placeholder='13:00 - 18:00'/>
+            </View>            
+          </View> 
+
+          <TouchableOpacity style={styles.botaoSolicitarAlteracao}>
+            <Text style={styles.botaoSolicitarAlteracaoTexto}>Solicitar Alteração Cadastral</Text>
+          </TouchableOpacity>
         
         </View>
 
@@ -62,23 +69,56 @@ const styles = StyleSheet.create({
   containerDadosCadastrais:{
     width:'80%',
     marginTop:'12%',
-    borderWidth:1,
-    borderColor:Constants.LARANJA
   },
 
   containerDadosCadastraisInput:{
-    width:'100%'
+    width:'100%',
+    marginBottom:'8%'
   },
 
   dadosCadastraisLabel:{
     color:Constants.MARROM,
     fontSize:16,
+    marginBottom:'2%'
   },
 
   dadosCadastraisInput:{
     width:'100%',
     borderWidth:1,
-    borderColor:Constants.LARANJA
+    borderColor:Constants.LARANJA,
+    borderRadius:Constants.ARREDONDAMENTO_BOTAO,
+    paddingVertical:'4%',
+    paddingHorizontal:'2%'
+  },
+
+  containerDadosCadastraisHorarios:{
+    width:'100%',
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+
+  dadosCadastraisHorariosInput:{
+    width:'48%',
+    borderWidth:1,
+    borderColor:Constants.LARANJA,
+    borderRadius:Constants.ARREDONDAMENTO_BOTAO,
+    paddingVertical:'4%',
+    paddingHorizontal:'2%'
+  },
+
+  botaoSolicitarAlteracao:{
+    width:'100%',
+    paddingVertical:'4%',
+    paddingHorizontal:'2%',
+    backgroundColor:Constants.LARANJA,
+    borderRadius:Constants.ARREDONDAMENTO_BOTAO
+  },
+
+  botaoSolicitarAlteracaoTexto:{
+    textAlign:'center',
+    color:Constants.BRANCO,
+    fontSize:16
   }
 
 })
