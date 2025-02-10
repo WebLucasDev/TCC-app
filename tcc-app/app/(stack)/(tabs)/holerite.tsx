6dcd0a834aeecf42d13ce7d6403c9a7342760adf
Lucas Venancio <weblucasdev@gmail.com>
2025-02-10 10:17:14 -0300

    commit 10-02-2025
    - Ajustes na tela de holerite

--- tcc-app/app/(stack)/(tabs)/holerite.tsx
+++ tcc-app/app/(stack)/(tabs)/holerite.tsx
@@ -1,10 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import Constants from '@/constants/Constants'
 import ContainerSuperior from '@/components/ContainerSuperior'
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function holerite() {
+  
+  /* Mês */
+  const [abertoMes, setAbertoMes] = useState(false);
+  const [valorSelecionadoMes, setValorSelecionadoMes] = useState(null);
+  const [meses, setMeses] = useState([
+    {label: 'Janeiro', value: 'Janeiro'},
+    {label: 'Fevereiro', value: 'Fevereiro'},
+    {label: 'Março', value: 'Março'},
+    {label: 'Abril', value: 'Abril'},
+    {label: 'Maio', value: 'Maio'},
+    {label: 'Junho', value: 'Junho'},
+    {label: 'Julho', value: 'Julho'},
+    {label: 'Agosto', value: 'Agosto'},
+    {label: 'Setembro', value: 'Setembro'},
+    {label: 'Outubro', value: 'Outubro'},
+    {label: 'Novembro', value: 'Novembro'},
+    {label: 'Dezembro', value: 'Dezembro'},
+  ]);
+
+  /* Mês */
+  const [abertoAno, setAbertoAno] = useState(false);
+  const [valorSelecionadoAno, setValorSelecionadoAno] = useState(null);
+  const [anos, setAnos] = useState([
+    {label: '2025', value: '2025'},
+    {label: '2024', value: '2024'},
+    {label: '2023', value: '2023'},
+    {label: '2022', value: '2022'},
+  ]);
 
   return (
     <SafeAreaProvider>
@@ -14,7 +43,27 @@ export default function holerite() {
         <ContainerSuperior/>
 
         <View style={styles.containerInputs}>
+          <DropDownPicker
+            open={abertoMes}
+            value={valorSelecionadoMes}
+            items={meses}
+            setOpen={setAbertoMes}
+            setValue={setValorSelecionadoMes}
+            setItems={setMeses}
+            placeholder='Mês'
+            style={styles.inputsAno}
+            />
 
+          <DropDownPicker
+            open={abertoAno}
+            value={valorSelecionadoAno}
+            items={anos}
+            setOpen={setAbertoAno}
+            setValue={setValorSelecionadoAno}
+            setItems={setAnos}
+            placeholder='Ano'
+            style={styles.inputs}
+            />
         </View>
 
         </View>
@@ -41,7 +90,7 @@ const styles = StyleSheet.create({
 
   containerInputs:{
     width:'80%',
-    height:70,
+    padding:'4%',
     marginTop:'12%',
     backgroundColor:Constants.MARROM,
     opacity:0.7,
@@ -52,6 +101,12 @@ const styles = StyleSheet.create({
     justifyContent:'center',
   },
 
-  
+  inputsAno:{
+    marginBottom:'4%',
+  },
+
+  inputs:{
+
+  } 
 
 })
